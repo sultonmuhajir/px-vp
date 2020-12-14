652335a8df4eb04c870db58c7cc1b95d23f24ae2
Sulton Muhajir <sulton.muhajir28@gmail.com>
2020-12-14 16:49:27 +0700

    initial commit

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function result(id, hsl) {
 
 
 // Px-Vw
-$("#px1").keyup(function () {
+$("#px1").on("keyup", () => {
     const px1 = parseFloat($("#px1").val());
     const temp1 = 100 * px1 / window.innerWidth;
     const hsl1 = temp1.toFixed(2);
@@ -30,7 +30,7 @@ $("#px1").keyup(function () {
 
 
 // Vw-Px
-$("#vw").keyup(function () {
+$("#vw").on("keyup", () => {
     const vw = parseFloat($("#vw").val());
     const temp2 = window.innerWidth * vw / 100;
     const hsl2 = temp2.toFixed(2);
@@ -39,7 +39,7 @@ $("#vw").keyup(function () {
 
 
 // Px-Vh
-$("#px2").keyup(function () {
+$("#px2").on("keyup", () => {
     const px2 = parseFloat($("#px2").val());
     const temp3 = 100 * px2 / window.innerHeight;
     const hsl3 = temp3.toFixed(2);
@@ -48,7 +48,7 @@ $("#px2").keyup(function () {
 
 
 // Vh-Px
-$("#vh").keyup(function () {
+$("#vh").on("keyup", () => {
     const vh = parseFloat($("#vh").val());
     const temp4 = window.innerHeight * vh / 100;
     const hsl4 = temp4.toFixed(2);
